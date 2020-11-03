7eab6bb8107d984702fea62a6e1d8e81441bc79d
lidiane-marques <marqueslidiane@gmail.com>
2020-11-03 20:14:58 -0300

    adição da rota mensagem

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.get('/',function(req,res){
    res.send("---OLÁ MUNDO---")
 })
 
-
+app.get('/mensagem',function(req,res){
+    res.send("essa mensagem é automatica")
+})
 
 app.listen(3000,function(){
     console.log("conexão inicialiada")
