f5c09d5b14d80b5cf517f55ec64dc4431fa0a8df
lidiane-marques <marqueslidiane@gmail.com>
2020-11-03 20:38:12 -0300

    adicionando a rota sobre

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/msg',function(req,res){
     res.send("essa mensagem é automatica")
 })
 
+app.get('/sobre',function(req,res){
+    res.send("essa pagina esta sendo desenvolvida por lidiane")
+})
+
+
 app.listen(3000,function(){
     console.log("conexão inicialiada")
 })
