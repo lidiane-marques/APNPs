95648bbee1b05062c42c83d18c6c58bb08c9c801
lidiane-marques <marqueslidiane@gmail.com>
2020-11-03 20:40:55 -0300

    criaçaoda rota layout

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/msg',function(req,res){
     res.send("essa mensagem é automatica")
 })
 
+app.get('/layout',function(req,res){
+    res.send("esse é o nove layout")
+})
+
 app.listen(3000,function(){
     console.log("conexão inicialiada")
 })
