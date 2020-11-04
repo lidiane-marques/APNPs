a44bd341026fda630aee18464bdfa47f004a493e
lidiane-marques <marqueslidiane@gmail.com>
2020-11-03 21:33:10 -0300

    configuraçoes iniciais

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require("express")
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine", "ejs")
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res){
    res.send("---OLÁ MUNDO---")
-})
-
-app.get('/msg',function(req,res){
-    res.send("essa mensagem é automatica")
-})
-
-app.get('/sobre',function(req,res){
-    res.send("essa pagina esta sendo desenvolvida por lidiane")
-})
-
-
-app.get('/layout',function(req,res){
-    res.send("esse é o nove layout")
 })
 
 app.listen(3000,function(){
